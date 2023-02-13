184af6baf7939cb861c3ba70b6bd9234dd94e299
Valery Zonov <valery.zonov@sibers.com>
2023-02-13 18:03:50 +0700

    classnames (add button)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
-import { Htag } from 'components';
+import { Button, Htag } from 'components';
 import React from 'react';
 
 export default function Home() {
   return (
     <div>
       <Htag tag='h1'>Text</Htag>
+      <Button appearance='primary'>Button</Button>
+      <Button appearance='ghost'>Button</Button>
     </div>
   );
 }
