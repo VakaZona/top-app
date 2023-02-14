be007198318aae008158e56eea340a1595a5b671
Valery Zonov <valery.zonov@sibers.com>
2023-02-14 16:04:22 +0700

    practice (add P tag)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Htag } from 'components';
+import { Button, Htag, P } from 'components';
 import React from 'react';
 
 export default function Home() {
@@ -7,6 +7,9 @@ export default function Home() {
       <Htag tag='h1'>Text</Htag>
       <Button appearance='primary' arrow='right'>Button</Button>
       <Button appearance='ghost' arrow='down'>Button</Button>
+      <P size='l'>Big text</P>
+      <P >Medium text</P>
+      <P size='s'>Small text</P>
     </div>
   );
 }
