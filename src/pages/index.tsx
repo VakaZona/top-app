b44afaa8e90088f58b2c069c5ae206d80298a957
Valery Zonov <valery.zonov@sibers.com>
2023-02-14 14:12:24 +0700

    add svg module

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <div>
       <Htag tag='h1'>Text</Htag>
-      <Button appearance='primary' className='test'>Button</Button>
-      <Button appearance='ghost'>Button</Button>
+      <Button appearance='primary' arrow='right'>Button</Button>
+      <Button appearance='ghost' arrow='down'>Button</Button>
     </div>
   );
 }
