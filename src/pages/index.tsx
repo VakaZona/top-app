58f58f6383302eede6ada2652e37c5a43c76ea49
Valery Zonov <valery.zonov@sibers.com>
2023-02-14 16:43:31 +0700

    component tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,22 @@
-import { Button, Htag, P } from 'components';
+import { Button, Htag, P, Tag } from 'components';
 import React from 'react';
 
 export default function Home() {
   return (
     <div>
       <Htag tag='h1'>Text</Htag>
+
       <Button appearance='primary' arrow='right'>Button</Button>
       <Button appearance='ghost' arrow='down'>Button</Button>
+
       <P size='l'>Big text</P>
       <P >Medium text</P>
       <P size='s'>Small text</P>
+
+      <Tag size='s'>Small tag</Tag>
+      <Tag size='m' color='red'>Red medium</Tag>
+      <Tag size='s' color='green' href='8.8.8.8'>Green</Tag>
+      <Tag color='primary'>Primary</Tag>
     </div>
   );
 }
