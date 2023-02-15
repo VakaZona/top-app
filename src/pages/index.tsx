4c0ff2a5b25ae382c4a6934fe92baaa12e046a4c
Valery Zonov <valery.zonov@sibers.com>
2023-02-15 16:41:00 +0700

    add Component Rating

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,26 @@
-import { Button, Htag, P, Tag } from 'components';
+import { Button, Htag, P, Rating, Tag } from 'components';
 import React, { useEffect, useState } from 'react';
 
 export default function Home() {
-  const [counter, setCounter] = useState<number>(0);
-
-  // Not use
-  // if (counter > 0) {
-  //   useEffect(() => {
-  //     console.log('Counter' + counter);
-  //     return function cleanup() {
-  //       console.log('Unmount');
-  //     };
-  //   });
-  // }
-
-  useEffect(() => {
-    if (counter > 0) {
-      console.log('Counter' + counter);
-    }
-    return function cleanup() {
-      console.log('Unmount');
-    };
-  });
-
+  const [rating, setRating] = useState<number>(4);
 
   return (
     <div>
-      <Htag tag='h1'>{counter}</Htag>
+      <Htag tag='h1'>Test</Htag>
 
-      <Button appearance='primary' arrow='right' onClick={() => setCounter(x => x + 1)}>Button</Button>
+      <Button appearance='primary' arrow='right'>Button</Button>
       <Button appearance='ghost' arrow='down'>Button</Button>
 
       <P size='l'>Big text</P>
       <P >Medium text</P>
       <P size='s'>Small text</P>
 
-      <Tag size='s'>Small tag</Tag>
+      <Tag size='s' >Small tag</Tag>
       <Tag size='m' color='red'>Red medium</Tag>
       <Tag size='s' color='green' href='8.8.8.8'>Green</Tag>
       <Tag color='primary'>Primary</Tag>
+
+      <Rating rating={rating} isEditable={true} setRating={setRating}/>
     </div>
   );
 }
