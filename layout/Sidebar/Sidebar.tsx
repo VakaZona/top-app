3bb4c4f00411e586835f60af94c9b743f03d7b33
Valery Zonov <valery.zonov@sibers.com>
2023-03-20 16:42:17 +0700

    structer routing

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -2,10 +2,13 @@ import { SidebarProps } from './Sidebar.ptops';
 import styles from './Sidebar.module.css';
 import cn from 'classnames';
 import { Menu } from 'layout/Menu/Menu';
+import Logo from '../logo.svg';
 
-export const Sidebar = ({ ...props }): JSX.Element => {
+export const Sidebar = ({ className, ...props }): JSX.Element => {
 	return (
-		<div {...props}>
+		<div className={cn(className, styles.sidebar) } {...props}>
+			<Logo />
+			<div>Search</div>
 			<Menu />
 		</div>
 	);
